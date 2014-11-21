692ae449d60274a61492b47a8ea7998f40d861c0
fanylv <fanylv@gmail.com>
2014-11-21 16:37:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/alumnot/lanzalamoneda/MyActivity.java
@@ -0,0 +1,64 @@
+package com.example.alumnot.lanzalamoneda;
+import android.support.v7.app.ActionBarActivity;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+
+public class MyActivity extends ActionBarActivity {
+    AnimationDrawable animacion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        animacion = (AnimationDrawable)getResources().getDrawable(R.drawable.lanzamiento);
+        ImageView iv = new ImageView(this);
+        iv.setBackgroundColor(Color.WHITE);
+        iv.setImageDrawable(animacion);
+
+        iv.setOnClickListener(new OnClickListener(){
+            public void onClick(View view) {
+                animacion.start();
+            }
+        });
+        setContentView(iv);
+        iv.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(animacion.isRunning()){
+                    animacion.stop();
+                }
+                else{
+                    animacion.start();
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
